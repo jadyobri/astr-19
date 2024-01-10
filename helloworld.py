1d02dea061b45436e2a9e044d6eaaaa04392bdcd
Jack O'Brien <112610774+jadyobri@users.noreply.github.com>
2024-01-09 16:27:14 -0800

    Added a hello world program
    This repository is now less boring!

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# This is a comment
+# This program will write Hello World!
+
+# This function tells the
+# computer to print Hello World
+def PrintHelloWorld():
+    print("Hello World!")
+
+# This is the program that will be
+# run by python
+def main():
+    PrintHelloWorld()
+
+# Executes the main program when script is run
+if __name__=="__main__":
+    main()
